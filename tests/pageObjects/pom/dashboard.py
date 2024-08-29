145afb238adc552b53080bdcf7cbb85802ba60a9
Kishor1390 <kishorshevkar1390@gmail.com>
2024-08-29 17:41:44 +0530

    Added files

---
+++ tests/pageObjects/pom/dashboard.py
@@ -0,0 +1,11 @@
+# Dashboard class
+
+#Steps to create a dashboard:
+#find user_logged_in > Page Locators
+#get user_logged_in text > Page Actions
+
+from selenium.webdriver.common.by import By
+
+class Dashboard:
+
+    pass
---
+++ tests/pageObjects/pom/login.py
@@ -0,0 +1,17 @@
+#-----Steps-----
+
+#Login Page Class
+#Page Locators
+#Page Actions
+
+#Webdriver INit
+#Custom Methods
+#No asserstions (in page object class)
+
+#get email and send keys - email
+#get password and send keys - password
+#case 1 - Click on submit button and navigate to dashboard page
+#case 2 - invalid > error message
+
+class LoginPage:
+    pass
